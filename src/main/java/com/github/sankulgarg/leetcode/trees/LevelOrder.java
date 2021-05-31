bef7890abca81cedcdb461cf2dd68606da4867ac
sgarg <sgarg@WCZC0067FSJ.dom01.frtg.root>
2021-05-31 16:46:26 +0300

    Trees: leetcode

---
+++ src/main/java/com/github/sankulgarg/leetcode/trees/LevelOrder.java
@@ -0,0 +1,36 @@
+package com.github.sankulgarg.leetcode.trees;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class LevelOrder {
+	
+	private void levelOrder(TreeNode node, Integer level, Map<Integer,List<Integer>> levelMap)
+	{
+		if (node == null)
+			return ;
+		
+		List<Integer> levelList= levelMap.computeIfAbsent(level, l-> new ArrayList<>());
+		
+		levelList.add(node.val);
+		 levelOrder(node.left, level + 1, levelMap);
+	        levelOrder(node.right, level + 1, levelMap);
+	}
+	
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		if (root == null)
+			return Collections.emptyList();
+
+		Map<Integer,List<Integer>> levelMap=new HashMap<>();
+		levelOrder(root, 0, levelMap);
+		return new ArrayList<>( levelMap.values());
+		
+		
+		
+		
+	}
+
+}
